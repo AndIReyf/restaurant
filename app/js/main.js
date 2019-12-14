826ab6fa7f79321950cd591ec7e530a29f925ae2
AndIReyf <andrey.prudius@hotmail.com>
2019-12-14 21:15:46 +0100

    Added adaptive version

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
 $(function () {
 
-  
+  $('.header__burger').click(function () {
+    $('.header__burger, .header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+
+  $('.slider__menu').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev">Previous offer</button>',
+    nextArrow: '<button type="button" class="slick-next">Next offer</button>',
+  });
+
+  $('.daily-menu__vegetarian span').on('click',function () {
+    $(this).toggleClass('active');
+    if ($('span').hasClass('active')) {
+      // $('.daily-menu__content.meat').toggleClass('turn-off');
+    }
+  });
 
 });
