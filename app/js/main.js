9d866a0760c1a48260789469ad015dc71ddde29f
AndIReyf <andrey.prudius@hotmail.com>
2019-12-14 22:08:09 +0100

    Fixed main menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function () {
 
   $('.header__burger').click(function () {
     $('.header__burger, .header__menu').toggleClass('active');
-    $('body').toggleClass('lock');
+    $('html, body').toggleClass('lock');
   });
 
 
